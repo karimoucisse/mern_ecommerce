a73930a78dddf5754ef84f1845c00971ed032f6d
karimou cisse <karimou.cisse1@gmail.com>
2024-08-03 20:28:17 +0200

    navlist component

---
+++ src/components/NavList.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const NavList = () => {
+	const List = [
+		{
+			name: "Accueil",
+            link: "/"
+		},
+		{
+			name: "Hommes",
+			link: "/men"
+		},
+		{
+			name: "Femmes",
+			link: "/woman"
+		},
+		{
+			name: "Enfants",
+			link: "/kids"
+		},
+	]
+  return (
+	<div class="flex gap-[50px]">
+		{List.map((element) => (
+			<div key={element.name}>
+                <a href={element.link}>{element.name}</a>
+            </div>
+		))}
+	</div>
+  )
+}
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { NavList } from './NavList'
 
 const Navbar = () => {
   return (
-	<div className='text-center w-full'>Navbar</div>
+	<div className='flex justify-around w-full p-[16px] '>
+		<div>Logo</div>
+		<NavList/>
+		<div>Left</div>
+	</div>
   )
 }
 
